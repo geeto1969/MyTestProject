609f0f1c3c1fe23856f758d144622d4401ec5d72
Kevin Robinson <kevin@family-robinson.com>
2014-08-16 09:29:26 -0500

    Created endpoint class.

---
+++ MyTestProject/src/endpoint/endpoint.py
@@ -0,0 +1,17 @@
+'''
+Created on Aug 16, 2014
+
+@author: Kevin
+'''
+
+class enpoint(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
